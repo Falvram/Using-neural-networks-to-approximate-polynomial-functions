c64785a0624f695caa4f942855a29f27fbda4e55
Falvram <wolflammen0@gmail.com>
2021-10-29 02:07:14 +0300

    Creating neural network to approximate polynomial functions

---
+++ main.py
@@ -0,0 +1,60 @@
+import math
+
+from sklearn.metrics import mean_squared_error
+from numpy import asarray
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from matplotlib import pyplot
+from sklearn.preprocessing import MinMaxScaler
+
+# CONSTANTS
+C = 1  # C > 0
+m = 3  # Degree of a polynomial
+eps = 0.01  # ε ∈ (0, 1/2)
+D = 1
+W = 16  # W ≤ 16 - network width
+
+# POLYNOMIAL FUNCTION
+x = asarray([i for i in range(-50, 51)])
+a = asarray([i for i in range(1, 100)])
+y = asarray([i ** 3 for i in x])
+
+# VARIABLES
+A = max(a)  # maximum of
+L = C * m * (math.log2(A) + math.log2(math.pow(eps, -1)) + m * math.log2(D) + math.log2(m))  # depth of network ≤ L
+
+# SCALE THE INPUT DATA
+x = x.reshape((len(x), 1))
+y = y.reshape((len(y), 1))
+scale_x = MinMaxScaler()
+x = scale_x.fit_transform(x)
+scale_y = MinMaxScaler()
+y = scale_y.fit_transform(y)
+
+# NETWORK TRAINING
+model = Sequential()
+model.add(Dense(W, input_dim=1, activation='relu'))
+for i in range(int(L)):
+    model.add(Dense(W, activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer='adam')
+model.fit(x, y, epochs=500, batch_size=10, verbose=0)
+
+# NETWORK PREDICT
+yhat = model.predict(x)
+
+# PLOT RESULTS
+x_plot = scale_x.inverse_transform(x)
+y_plot = scale_y.inverse_transform(y)
+yhat_plot = scale_y.inverse_transform(yhat)
+# report model error
+print('MSE: %.3f' % mean_squared_error(y_plot, yhat_plot))
+# plot x vs y
+pyplot.scatter(x_plot, y_plot, label='Actual')
+# plot x vs yhat
+pyplot.scatter(x_plot, yhat_plot, label='Predicted')
+pyplot.title('Input (x) versus Output (y)')
+pyplot.xlabel('Input Variable (x)')
+pyplot.ylabel('Output Variable (y)')
+pyplot.legend()
+pyplot.show()
