584595cf2dfadf7316785163ece904ce2b6d4c84
Falvram <wolflammen0@gmail.com>
2021-12-06 19:31:01 +0300

    Successfully approximating polynomial functions with Brauer, Yao's and Standard algorithms

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import math
-import random
-
 import sympy
 import time
 import numpy
 import tensorflow as tf
-from keras.callbacks import ReduceLROnPlateau
 from sklearn.metrics import mean_squared_error
 from numpy import asarray, arange
 from tensorflow.keras.models import Sequential
@@ -13,11 +10,11 @@ from tensorflow.keras.layers import Dense
 from matplotlib import pyplot
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.optimizers import SGD
 
 
 class SaveBestModel(tf.keras.callbacks.Callback):
     def __init__(self, save_best_metric='val_loss', this_max=False):
+        self.best_weights = None
         self.save_best_metric = save_best_metric
         self.max = this_max
         if this_max:
@@ -31,39 +28,48 @@ class SaveBestModel(tf.keras.callbacks.Callback):
             if metric_value > self.best:
                 self.best = metric_value
                 self.best_weights = self.model.get_weights()
-
         else:
             if metric_value < self.best:
                 self.best = metric_value
                 self.best_weights = self.model.get_weights()
 
 
-def train_predict_model(x, y, m, absolute_power):
+def train_predict_model(x, y, m, power_or_poly):
     # CONSTANTS
     C = 1  # C > 0
-    # m = 1  # Degree of a polynomial
     eps = 0.01  # ε ∈ (0, 1/2)
     D = 1  # the boundaries of the segment in which we perform the approximation
     W = 16  # W ≤ 16 - network width
 
     # VARIABLES
-    # A = 1  # maximum of factor
     if m == 0:
         m = 1
-    Lc = int(C * m * (math.log2(A) + math.log2(math.pow(eps, -1)) + m * math.log2(D) + math.log2(m))) # depth of network ≤ L
+    Lc = int(
+        C * m * (math.log2(A) + math.log2(math.pow(eps, -1)) + m * math.log2(D) + math.log2(m)))  # depth of network ≤ L
     if Lc > 30:
         L = 30
     else:
         L = Lc
-    print("A, m, Depth:", A, m, int(L))
+    epochs = 200
 
     # TRAINING DATA
-    var_x = []
+    var_x = [0, 1, -1]
     var_x = numpy.append(var_x, x)
-    var_x = numpy.append(var_x, numpy.random.rand(1, 1310720))
-    var_x = numpy.append(var_x, -numpy.random.rand(1, 131072))
-    var_x = var_x.reshape(len(var_x), 1)
-    var_y = asarray([i ** absolute_power for i in var_x])
+    var_x = numpy.append(var_x, numpy.random.rand(1, 131072))
+    var_x = numpy.append(var_x, -numpy.random.rand(1, 131072 - len(var_x) % 131072))
+    numpy.random.shuffle(var_x)
+    if isinstance(power_or_poly, sympy.polys.polytools.Poly):
+        var_y = asarray([0 for i in var_x])
+        for i in range(len(power_or_poly.coeffs())):
+            var_y = numpy.add(var_y, asarray([power_or_poly.coeffs()[i] * k **
+                                              power_or_poly.monoms()[i][0] for k in var_x]))
+        var_x = var_x.reshape(len(var_x), 1)
+        var_y = var_y.reshape(len(var_y), 1)
+    else:
+        if power_or_poly % 2 == 1:
+            epochs = 500
+        var_x = var_x.reshape(len(var_x), 1)
+        var_y = asarray([i ** power_or_poly for i in var_x])
 
     # SCALE THE INPUT DATA
     scale_var_x = MinMaxScaler()
@@ -80,24 +86,25 @@ def train_predict_model(x, y, m, absolute_power):
     # NETWORK TRAINING
     model = Sequential()
     model.add(Dense(W, input_dim=1, activation='relu'))
-    for i in range(int(L) - 1):
-        model.add(Dense(int(W), activation='relu'))
+    for i in range(L - 1):
+        model.add(Dense(W, activation='relu'))
     model.add(Dense(1))
-    starter_learning_rate = 0.0001
-    end_learning_rate = 0.00001
-    decay_steps = 10000
     learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(
-        starter_learning_rate,
-        decay_steps,
-        end_learning_rate,
+        initial_learning_rate=0.0001,
+        decay_steps=10000,
+        end_learning_rate=0.00001,
         power=0.5)
-    model.compile(loss='mse', optimizer=Adam(learning_rate=learning_rate_fn), metrics=['binary_accuracy'])
+    model.compile(loss='mse',
+                  optimizer=Adam(learning_rate=learning_rate_fn))
     save_best_model = SaveBestModel()
-    history = model.fit(var_x, var_y, validation_split=0.2, epochs=int(2 * math.log2(Lc) * Lc) + 100, batch_size=131072, verbose=0,
+    history = model.fit(var_x, var_y,
+                        validation_split=0.2,
+                        epochs=int(math.log2(Lc) * Lc) + epochs,
+                        batch_size=131072,
+                        verbose=0,
                         callbacks=[save_best_model])
     model.set_weights(save_best_model.best_weights)
-    # print(history.history.keys())
-    show_acc_loss(history)
+    show_loss(history)
 
     # NETWORK PREDICT
     yhat = model.predict(x)
@@ -107,10 +114,10 @@ def train_predict_model(x, y, m, absolute_power):
     y = scale_y.inverse_transform(y)
     yhat = scale_y.inverse_transform(yhat)
     # report model error
-    print('MSE: %.3f' % mean_squared_error(y, yhat))
+    print('MSE: %.5f' % mean_squared_error(y, yhat))
     plot(x, y, yhat)
     yhat = yhat.reshape(1, len(yhat))
-    return yhat[0], absolute_power
+    return yhat[0]
 
 
 def plot(actual_x, actual_y, predicted_y):
@@ -118,21 +125,14 @@ def plot(actual_x, actual_y, predicted_y):
     pyplot.scatter(actual_x, actual_y, label='Actual')
     # plot x vs yhat
     pyplot.scatter(actual_x, predicted_y, label='Predicted')
-    pyplot.title('Input (x) versus Output (y)')
+    pyplot.title(title_alg + title_func)
     pyplot.xlabel('Input Variable (x)')
     pyplot.ylabel('Output Variable (y)')
     pyplot.legend()
     pyplot.show()
 
 
-def show_acc_loss(history):
-    # pyplot.plot(history.history['binary_accuracy'])
-    # pyplot.plot(history.history['val_binary_accuracy'])
-    # pyplot.title('model accuracy')
-    # pyplot.ylabel('accuracy')
-    # pyplot.xlabel('epoch')
-    # pyplot.legend(['train', 'test'], loc='upper left')
-    # pyplot.show()
+def show_loss(history):
     # summarize history for loss
     pyplot.plot(history.history['loss'])
     pyplot.plot(history.history['val_loss'])
@@ -144,10 +144,14 @@ def show_acc_loss(history):
 
 
 def evaluate_two_power_n(y, n, absolute_power):
+    global title_func
+    title_func = ""
     y_predicted = y
     for i in range(n):
+        absolute_power *= 2
+        title_func = "y = x^%d" % absolute_power
         y = asarray([k ** 2 for k in y])
-        y_predicted, absolute_power = train_predict_model(x, y, 2, absolute_power * 2)
+        y_predicted = train_predict_model(x, y, 2, absolute_power)
     return y_predicted, absolute_power
 
 
@@ -167,24 +171,35 @@ def n_in_radix_two_k(num, k):
 
 
 def brauer(coeff, power):
+    global title_alg, title_func
+    title_alg = "Brauer algorithm, "
     k = 3
     res_list = n_in_radix_two_k(power, k)
     print(res_list)
     res = asarray([1 for k in x])
-    absolute_power = 1
+    if power == 0:
+        res = asarray([coeff * i for i in res])
+        y = asarray([coeff * i ** power for i in x])
+        title_func = "y = %d" % coeff
+        plot(x, y, res)
+        return res.reshape(1, len(res))
     for i in range(len(res_list)):
-        y = asarray([k ** res_list[i][0] for k in x])
-        y_predicted, absolute_power = train_predict_model(x, y, res_list[i][0], res_list[i][0])
+        absolute_power = res_list[i][0]
+        title_func = "y = x^%d" % res_list[i][0]
+        y = asarray([k ** absolute_power for k in x])
+        y_predicted = train_predict_model(x, y, absolute_power, absolute_power)
         answ, absolute_power = evaluate_two_power_n(y_predicted, res_list[i][1], absolute_power)
-        answ = answ[0]
         res = numpy.multiply(res, answ)
-    res, absolute_power = train_predict_model(x, asarray([coeff * i for i in res]), 1, absolute_power)
+    res = asarray([coeff * i for i in res])
     y = asarray([coeff * i ** power for i in x])
+    title_func = "y = x^%d" % power
     plot(x, y, res)
     return res.reshape(1, len(res))
 
 
 def yao_s(coeff, power):
+    global title_alg, title_func
+    title_alg = "Yao's algorithm, "
     k = 3
     factorization = []
     res = n_in_radix_two_k(power, k)
@@ -201,10 +216,18 @@ def yao_s(coeff, power):
         for k in range(len(factorization[i][1])):
             temp, absolute_power = evaluate_two_power_n(y, factorization[i][1][k], 1)
             term = numpy.multiply(term, temp)
-        term, absolute_power = train_predict_model(x, asarray([j ** factorization[i][0] for j in term]),
-                                                   factorization[i][0], absolute_power * factorization[i][0])
+        title_func = ""
+        title_func = "y = x^%d" % absolute_power * factorization[i][0]
+        term = train_predict_model(x, asarray([j ** factorization[i][0] for j in term]),
+                                   factorization[i][0], absolute_power * factorization[i][0])
         answ = numpy.multiply(answ, term)
-    answ, absolute_power = train_predict_model(x, asarray([coeff * j for j in answ]), 1, absolute_power)
+    answ = asarray([coeff * j for j in answ])
+    y = asarray([coeff * i ** power for i in x])
+    if factorization:
+        title_func = "y = x^%d" % power
+    else:
+        title_func = "y = %d" % coeff
+    plot(x, y, answ)
     return answ
 
 
@@ -224,21 +247,38 @@ def parse_function(function):
 
 
 def approximate_function(function):
+    global title_alg, title_func
     print("Approximating function", function)
     coeffs, monoms, poly = parse_function(function)
     brauer_res = asarray([0 for i in x])
     yao_s_res = asarray([0 for i in x])
     actual_res = asarray([0 for i in x])
+    brauer_time = 0
+    yao_s_time = 0
+    standard_time = 0
     for i in range(len(coeffs)):
+        start_time = time.time()
         brauer_res = numpy.add(brauer_res, brauer(coeffs[i], monoms[i][0]))
+        brauer_time += time.time() - start_time
+        start_time = time.time()
         yao_s_res = numpy.add(yao_s_res, yao_s(coeffs[i], monoms[i][0]))
+        yao_s_time += time.time() - start_time
         actual_res = numpy.add(actual_res, asarray([coeffs[i] * k ** monoms[i][0] for k in x]))
-    predicted_y, c = train_predict_model(x, actual_res, poly.degree(), poly.degree())
-    print("Comparing using Brauer predicted function with actual function")
+    title_alg = "Standard algorithm, "
+    title_func = function
+    start_time = time.time()
+    predicted_y = train_predict_model(x, actual_res, poly.degree(), poly)
+    standard_time += time.time() - start_time
+    print("Comparing using Brauer predicted function with actual function, time = %s seconds" % brauer_time)
+    title_alg = "Brauer algorithm, "
+    title_func = function
     plot(x, actual_res, brauer_res)
-    print("Comparing using Yao's predicted function with actual function")
+    print("Comparing using Yao's predicted function with actual function, time = %s seconds" % yao_s_time)
+    title_alg = "Yao's algorithm, "
     plot(x, actual_res, yao_s_res)
-    print("Comparing using Standard predicted function with actual function")
+    print("Comparing using Standard predicted function with actual function, time = %s seconds" % standard_time)
+    title_alg = "Standard algorithm, y = "
+    title_func = function
     plot(x, actual_res, predicted_y)
 
 
@@ -252,11 +292,14 @@ def test_two_n_numbers(n):
     evaluate_two_power_n(y, n, 1)
     print("--- %s seconds ---" % (time.time() - start_time))
     start_time = time.time()
-    train_predict_model(x, asarray([i ** power for i in x]), power, power)
+    train_predict_model(x, asarray([i ** power for i in x]), 2, power)
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
 x = asarray([i for i in arange(-1, 1.00001, 0.00001)])
+title_alg = ""
+title_func = ""
+A = 1
 func = "15 * x ^ 51 + 9 * x ^ 2 + 34"
-# approximate_function(func)
-test_two_n_numbers(10)
+approximate_function(func)
+# test_two_n_numbers(6)
